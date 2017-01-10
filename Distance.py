74fc1058532e102e81486753cea2f05cbc173b46
Benny Mirisola <bennymirisola@gmail.com>
2017-01-10 15:57:06 -0500

    Added Distance Tracking

---
+++ Distance.py
@@ -0,0 +1,9 @@
+import Tape
+import cv2
+import numpy as np
+
+def focal_length(image, width, distance):
+    return (image * distance)/width
+
+def distance(focalLength, width, image):
+    return (focalLength* width)/ image
