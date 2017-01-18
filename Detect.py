952db3ecfbad8735687aa008d8bf8a9e72d4716f
Benny Mirisola <bennymirisola@gmail.com>
2017-01-17 19:09:24 -0500

    Finished object detection. Drwas circle around object. center variable gives x and y coordinates of center circle

--- Detect.py
+++ Detect.py
@@ -27,6 +27,7 @@ cv2.setMouseCallback('hsv', printpix)
 #Calibrated HSV Ranges
 lower_green = np.array([0,0,230])
 upper_green = np.array([255,255,255])
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 #Starts video capture
 #Creates two video windows. One from camera feed. Other blacks out everything not between calibrated BGR ranges
@@ -37,12 +38,35 @@ while(True):
     mask = cv2.inRange(hsv, lower_green, upper_green)
     res = cv2.bitwise_and(frame,frame,mask=mask)
 
+    '''
     # contours:
     im2, contours, hierarchy = cv2.findContours(mask, 2, 1)  # without 'im2' for opencv2.4 !!
     if len(contours) > 0:
         cnt = contours[0]
         (x, y, w, h) = cv2.boundingRect(cnt)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
+    '''
+
+
+    im2, contours, hierarchy = cv2.findContours(mask, 2, 1)
+    for c in contours:
+        # find minimum area
+        #rect = cv2.minAreaRect(c)
+        # calculate coordinates of the minimum area rectangle
+        #box = cv2.boxPoints(rect)
+        # normalize coordinates to integers
+        #box = np.int0(box)
+        # draw contours
+        #cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
+        # calculate center and radius of minimum enclosing circle
+        (x, y), radius = cv2.minEnclosingCircle(c)
+        # cast to integers
+        center = (int(x), int(y))
+        radius = int(radius)
+        # draw the circle
+        print center
+        img = cv2.circle(frame, center, radius, (0, 255, 0), 2)
+        #cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
 
     cv2.imshow('orig',frame)
     cv2.imshow('fff',res)
