4ee1e6f8b8edd0764abc76b57f58d55ee11571b7
Matt Turi <mturi@mort11.org>
2017-01-18 21:55:09 -0500

    Added UDP socket code

--- Detect.py
+++ Detect.py
@@ -1,42 +1,46 @@
-import cv2
-import numpy as np
 import os
 import time
-import Rectangle
 
-#Creates video capture object from camera
+import cv2
+import numpy as np
+
+from UDPCannon import UDPCannon
+
 vid = cv2.VideoCapture(1)
 time.sleep(1)
 
-os.system("/home/bennym/Desktop/turnonautoexposure.sh")
-#os.system("/home/bennym/Desktop/turnoffautoexposure.sh")
-os.system("/home/bennym/Desktop/all.sh")
+# os.system("/scripts/turnonautoexposure.sh")
+os.system("scripts/turnoffautoexposure.sh")
+# os.system("scripts/all.sh")
 
-#detects double left click and stores the coordinates in px
-#This is for calibrating pixel values of retro tape so that everything can be blocked out
-def printpix(event,x,y, flags, params):
+socket = UDPCannon("10.0.11.67", 8090)
+
+
+# detects double left click and stores the coordinates in px
+# This is for calibrating pixel values of retro tape so that everything can be blocked out
+def printpix(event, x, y, flags, params):
     global hsv
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        print hsv[y,x]
+        print hsv[y, x]
 
-#Sets printpix function frame titled 'orig'
+
+# Sets printpix function frame titled 'orig'
 cv2.namedWindow('hsv')
 cv2.setMouseCallback('hsv', printpix)
 
-
-#Calibrated HSV Ranges
-lower_green = np.array([0,0,230])
-upper_green = np.array([255,255,255])
+# Calibrated HSV Ranges
+lower_green = np.array([0, 0, 230])
+upper_green = np.array([255, 255, 255])
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#Starts video capture
-#Creates two video windows. One from camera feed. Other blacks out everything not between calibrated BGR ranges
+# Starts video capture
+# Creates two video windows. One from camera feed. Other blacks out everything not between calibrated BGR ranges
 
-while(True):
+while (True):
     ret, frame = vid.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_green, upper_green)
-    res = cv2.bitwise_and(frame,frame,mask=mask)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
 
     '''
     # contours:
@@ -47,17 +51,16 @@ while(True):
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
     '''
 
-
     im2, contours, hierarchy = cv2.findContours(mask, 2, 1)
     for c in contours:
         # find minimum area
-        #rect = cv2.minAreaRect(c)
+        # rect = cv2.minAreaRect(c)
         # calculate coordinates of the minimum area rectangle
-        #box = cv2.boxPoints(rect)
+        # box = cv2.boxPoints(rect)
         # normalize coordinates to integers
-        #box = np.int0(box)
+        # box = np.int0(box)
         # draw contours
-        #cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
+        # cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
         # calculate center and radius of minimum enclosing circle
         (x, y), radius = cv2.minEnclosingCircle(c)
         # cast to integers
@@ -66,15 +69,16 @@ while(True):
         # draw the circle
         print center
         img = cv2.circle(frame, center, radius, (0, 255, 0), 2)
-        #cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
+        # cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
+        socket.put("centerX", str(center))
 
-    cv2.imshow('orig',frame)
-    cv2.imshow('fff',res)
+    cv2.imshow('orig', frame)
+    cv2.imshow('fff', res)
     cv2.imshow("hsv", hsv)
     k = cv2.waitKey(20) & 0xFF
     if k == ord('q'):
         break
 
-#exits
+# exits
 vid.release()
 cv2.destroyAllWindows()
--- Tape.py
+++ Tape.py
@@ -1,42 +1,45 @@
 import cv2
 import numpy as np
 
-#Creates video capture object from camera
+# Creates video capture object from camera
 vid = cv2.VideoCapture(1)
 
-#X and Y coordinates of mouseclick
-coX, coY = 0,0
+# X and Y coordinates of mouseclick
+coX, coY = 0, 0
 
-#pixel array that will hold BGR values of the pixel at X and Y
+# pixel array that will hold BGR values of the pixel at X and Y
 px = []
 
-#detects double left click and stores the coordinates in px
-#This is for calibrating pixel values of retro tape so that everything can be blocked out
-def printpix(event,x,y, flags, params):
+
+# detects double left click and stores the coordinates in px
+# This is for calibrating pixel values of retro tape so that everything can be blocked out
+def printpix(event, x, y, flags, params):
     global coX, coY, px
     if event == cv2.EVENT_LBUTTONDBLCLK:
         coX, coY = x, y
         px = frame[coY, coX]
 
-#Sets printpix function frame titled 'orig'
+
+# Sets printpix function frame titled 'orig'
 cv2.namedWindow('orig')
 cv2.setMouseCallback('orig', printpix)
 
-#Calibrated pixel ranges
-lower_green = np.array([5,10,20])
-upper_green = np.array([100,75,40])
+# Calibrated pixel ranges
+lower_green = np.array([5, 10, 20])
+upper_green = np.array([100, 75, 40])
 
-#Starts video capture
-#Creates two video windows. One from camera feed. Other blacks out everything not between calibrated BGR ranges
+# Starts video capture
+# Creates two video windows. One from camera feed. Other blacks out everything not between calibrated BGR ranges
 
-while(True):
+while (True):
     ret, frame = vid.read()
-    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #mask = cv2.inRange(hsv, lower_green, upper_green)
-    #res = cv2.bitwise_and(frame,frame,mask=mask)
-    cv2.imshow('orig',frame)
-    #cv2.imshow('fff',res)
-    #if 'a' key is pressed BGR values are printed. If 'q' is pressed while loop breaks
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # mask = cv2.inRange(hsv, lower_green, upper_green)
+    # res = cv2.bitwise_and(frame,frame,mask=mask)
+    cv2.imshow('orig', frame)
+    # cv2.imshow('fff',res)
+
+    # if 'a' key is pressed BGR values are printed. If 'q' is pressed while loop breaks
     k = cv2.waitKey(20) & 0xFF
     if k == ord('q'):
         break
@@ -44,6 +47,6 @@ while(True):
     elif k == ord('a'):
         print px
 
-#exits
+# exits
 vid.release()
 cv2.destroyAllWindows()
