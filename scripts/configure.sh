78816bf8c0175dceb8d451ff29be75f9460f53f8
Matt Turi <mturi@mort11.org>
2017-01-19 18:47:52 -0500

    Fixed configuration

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -2,5 +2,7 @@ v4l2-ctl -d /dev/video1 --set-ctrl=brightness=0
 v4l2-ctl -d /dev/video1 --set-ctrl=contrast=200
 v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature_auto=0
 v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature=6400
+v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto=1
+v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto_priority=0
 v4l2-ctl -d /dev/video1 --set-ctrl=exposure_absolute=3
 v4l2-ctl -d /dev/video1 --set-ctrl=focus_auto=0
