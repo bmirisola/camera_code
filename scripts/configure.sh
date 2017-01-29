371e45fd118ef12e8d5044112bd76f26c5c046e9
Benny Mirisola <bennymirisola@gmail.com>
2017-01-29 14:34:57 -0500

    added everything back

---
+++ scripts/configure.sh
@@ -0,0 +1,17 @@
+v4l2-ctl -d /dev/video1 --set-ctrl=brightness=0
+v4l2-ctl -d /dev/video1 --set-ctrl=contrast=255
+v4l2-ctl -d /dev/video1 --set-ctrl=saturation=255
+v4l2-ctl -d /dev/video1 --set-ctrl=gain=0
+v4l2-ctl -d /dev/video1 --set-ctrl=sharpness=0
+v4l2-ctl -d /dev/video1 --set-ctrl=backlight_compensation=0
+v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature_auto=0
+v4l2-ctl -d /dev/video1 --set-ctrl=white_balance_temperature=6500
+v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto=1
+v4l2-ctl -d /dev/video1 --set-ctrl=exposure_auto_priority=0
+v4l2-ctl -d /dev/video1 --set-ctrl=exposure_absolute=1
+v4l2-ctl -d /dev/video1 --set-ctrl=focus_auto=0
+v4l2-ctl -d /dev/video1 --set-ctrl=focus_absolute=0
+#v4l2-ctl -d /dev/video1 --set-ctrl=power_line_frequency=2
+#v4l2-ctl -d /dev/video1 --set-ctrl=led1_mode=3
+#v4l2-ctl -d /dev/video1 --set-ctrl=led1_frequency=0
+
