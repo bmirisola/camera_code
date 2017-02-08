b40d8eabf425169048649ec5e1212b15b9d9da75
Benny Mirisola <bennymirisola@gmail.com>
2017-02-07 21:16:53 -0500

    added Polygon objects

--- Detect.py
+++ Detect.py
@@ -4,10 +4,8 @@ import time
 
 import cv2
 
-global frame, mask
 import Constants
-import Distance
-import Polygon
+from Polygon import Polygon
 from UDPCannon import UDPCannon
 
 vid = cv2.VideoCapture(Constants.camera_port)
@@ -16,9 +14,6 @@ time.sleep(1)
 os.system("scripts/configure.sh")
 socket = UDPCannon("10.0.11.67", 8090)
 tape_contour = []
-
-bottom_tape = Polygon.Polygon.make_polygon(frame, mask)
-top_tape = Polygon.Polygon.make_polygon(frame, mask)
 
 # detects double left click and stores the coordinates in px
 # This is for calibrating pixel values of retro tape so that everything can be blocked out
@@ -50,6 +45,9 @@ while (True):
     mask = cv2.inRange(hsv, lower_green, upper_green)
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
+    bottom_tape = Polygon.make_polygon(Polygon(frame, mask), frame, mask)
+    top_tape = Polygon.make_polygon(Polygon(frame, mask), frame, mask)
+
     '''
     # contours:
     im2, contours, hierarchy = cv2.findContours(mask, 2, 1)  # without 'im2' for opencv2.4 !!
@@ -61,47 +59,11 @@ while (True):
     bottom_tape.run()
     top_tape.run()
 
-    im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for c in contours:
-
-        # find minimum area
-        rect = cv2.minAreaRect(c)
-        # calculate coordinates of the minimum area rectangle
-        box = cv2.boxPoints(rect)
-        # normalize coordinates to integers
-        box = np.int0(box)
-        # draw contours
-        #cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
-        # calculate center and radius of minimum enclosing circle
-        (x, y), radius = cv2.minEnclosingCircle(c)
-
-        # cast to integers
-        center = (int(x), int(y))
-        radius = int(radius)
-        distance = Distance.find_distance(Constants.focal_length, Constants.tape_width, radius * 2)
-        meters = distance * 0.0254
-        # draw the circle
-        # print center
-        # print 'meters are: ' + str(meters)
-        epsilon = 0.000001 * cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, epsilon, True)
-
-        print 'Approx is' + str(len(approx))
-        if len(approx) > 4:
-            tape_contour.append(approx)
-
-        if (len(tape_contour) > 0 and len(tape_contour) < 2):
-            for x in range(0, len(tape_contour)):
-                cv2.drawContours(frame, tape_contour, 0, (0, 0, 255), 2)
-                cv2.circle(frame, center, radius, (0, 255, 0), 2)
-
-        # print len(tape_contour)
-
-        try:
-            socket.put("centerX", str(center))
-            socket.put("distanceMeters", str(meters))
-        except Exception as e:
-            print "Can't connect : {0}".format(e)
+    try:
+        socket.put("centerX", str(bottom_tape.center))
+        # socket.put("distanceMeters", str(meters))
+    except Exception as e:
+        print "Can't connect : {0}".format(e)
 
     cv2.imshow('orig', frame)
     # cv2.imshow('mask', mask)
--- Polygon.py
+++ Polygon.py
@@ -10,18 +10,19 @@ class Polygon:
     ratio = 0
     center = None
 
-    def __init__(self, mask, frame):
+    def __init__(self, frame, mask):
         self.mask = mask
         self.frame = frame
 
-    def make_polygon(mask, frame):
-        polygon = Polygon(mask, frame)
+    def make_polygon(self, frame, mask):
+        polygon = Polygon(frame, mask)
         return polygon
 
-    im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     def run(self):
-        global contours, tape_contour, frame
+
+        # global contours, tape_contour, frame, mask
+        im2, contours, hierarchy = cv2.findContours(self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for c in contours:
             (x, y), radius = cv2.minEnclosingCircle(c)
 
@@ -36,9 +37,9 @@ class Polygon:
             approx = cv2.approxPolyDP(c, epsilon, True)
 
             if len(approx) > 4:
-                tape_contour.append(approx)
+                self.tape_contour.append(approx)
 
-            if (len(tape_contour) > 0 and len(tape_contour) < 2):
-                for x in range(0, len(tape_contour)):
-                    cv2.drawContours(frame, tape_contour, 0, (0, 0, 255), 2)
-                    cv2.circle(frame, center, radius, (0, 255, 0), 2)
+            if (len(self.tape_contour) > 0 and len(self.tape_contour) < 2):
+                for x in range(0, len(self.tape_contour)):
+                    cv2.drawContours(self.frame, self.tape_contour, 0, (0, 0, 255), 2)
+                    cv2.circle(self.frame, center, radius, (0, 255, 0), 2)
