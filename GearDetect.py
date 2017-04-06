516aa6ea9948f53e246afc90ccb4631afd2a04c3
Benny Mirisola <bennymirisola@gmail.com>
2017-04-06 12:21:24 -0400

    added negative angle

--- GearDetect.py
+++ GearDetect.py
@@ -59,9 +59,11 @@ upper_green = np.array([Constants.upper_blue, Constants.upper_green, Constants.u
 # Creates two video windows. One from camera feed. Other blacks out everything not between calibrated BGR ranges
 while (True):
     ret, frame = vid.read()
+    # frame = cv2.flip(frame,0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_green, upper_green)
     res = cv2.bitwise_and(frame, frame, mask=mask)
+    # res = cv2.flip(res,0)
 
     # finds contours
     im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -92,11 +94,13 @@ while (True):
             angle_rads = math.atan(ConstantsGear.gear_peg_with_tape_length / HorizontalDistance)
             angle_deg = math.degrees(angle_rads)
             angle_deg = math.ceil(angle_deg)
-        print ('The angle is ' + str(angle_deg))
-        print ('The distance is ' + str(Distance.find_distance(ConstantsGear.fake_focal, 4, top_tape.radius)))
-        print("The left tape center is: " + str(top_tape.center))
-        print("The right tape center is: " + str(bottom_tape.center))
-        print ('The center is:' + str(center))
+            if (center[0] < 320):
+                angle_deg = -angle_deg
+                # print ('The angle is ' + str(angle_deg))
+                # print ('The distance is ' + str(Distance.find_distance(ConstantsGear.fake_focal, 4, top_tape.radius)))
+                # print("The left tape center is: " + str(top_tape.center))
+                # print("The right tape center is: " + str(bottom_tape.center))
+                # print ('The center is:' + str(center))
 
     # print Distance.focal_length(bottom_tape.radius,2,158)
     # Finds horizontal distance
@@ -111,7 +115,7 @@ while (True):
     # Shows Frames
     cv2.imshow('orig', frame)
     cv2.imshow('mask', mask)
-    # cv2.imshow('res', res)
+    cv2.imshow('res', res)
     cv2.imshow("hsv", hsv)
 
     # Sets all values back to default
