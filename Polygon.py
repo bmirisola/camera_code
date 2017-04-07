702061205d768a54bc9c3639cd2de259f1389614
Benny Mirisola <bennymirisola@gmail.com>
2017-04-06 22:29:05 -0400

    added finding highest polygon area to polygon class

--- Polygon.py
+++ Polygon.py
@@ -7,6 +7,9 @@ class Polygon:
     frame = None
     radius = 0
     center = []
+    tape = [None] * 2
+    high = 0
+    areas = []
 
     #Contstructor to make Polygon objects
     def __init__(self, frame, mask):
@@ -31,12 +34,20 @@ class Polygon:
         #Add Polygons to tape contour if they are of point size 4 or greater
         if len(approx) > 4:
             self.tape_contour.append(approx)
+            self.areas.append(cv2.contourArea(approx))
+
+        for x in len(self.areas):
+            if self.areas[x] > self.areas[x + 1]:
+                self.high = x
+
+        cv2.drawContours(self.frame, self.tape_contour[self.high], 0, (0, 0, 255), 2)
+        cv2.circle(self.frame, self.center, self.radius, (0, 255, 0), 2)
 
         #draw all detected polygons
-        if (len(self.tape_contour) > 0 and len(self.tape_contour) < 2):
-            for x in range(0, len(self.tape_contour)):
-                cv2.drawContours(self.frame, self.tape_contour, 0, (0, 0, 255), 2)
-                cv2.circle(self.frame, self.center, self.radius, (0, 255, 0), 2)
+        # if (len(self.tape_contour) > 0 and len(self.tape_contour) < 2):
+        # for x in range(0, len(self.tape_contour)):
+        # cv2.drawContours(self.frame, self.tape_contour, 0, (0, 0, 255), 2)
+        # cv2.circle(self.frame, self.center, self.radius, (0, 255, 0), 2)
 
         #converts center to list from tuple
         self.center = list(self.center)
